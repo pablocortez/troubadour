b610b823ee165072cd22ddc70a3b099bd37499db
Pablo Cortez <pableocortez07@gmail.com>
2016-07-13 16:10:02 -0700

    Updated stream functionality

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));     //serve static assets
 app.get('*', function(req, res) {
-    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.listen(port, function() {
